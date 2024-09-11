4e2c3d91272990e66863fd894bfe219a7b9765af
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-09-11 22:54:54 +0530

    add links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import "../styles/footer.css"
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className="footer-container">
+    <div id="footer" className="footer-container">
       <div className="footer">
         <div className="footer-content">
           {/* <div className="newsletter">
@@ -26,33 +27,36 @@ const Footer = () => {
             <div className="footer-column">
               <div className="footer-links">
                 <h3>Useful Links</h3>
-                <a href="#">Home</a>
-                <a href="#">About</a>
+                <Link to="/home">Home</Link>
+                <a href="#footer">About</a>
                 <a href="#">Products & Services</a>
                 <a href="#">Client Testimonials</a>
-                <a href="#">Contact Us</a>
+                <a href="#footer">Contact Us</a>
               </div>
             </div>
             <div className="footer-column">
               <div className="footer-links">
                 <h3>Service Area</h3>
-                <a href="#">Clean Vision Solutions</a>
-                <a href="#">Ministy of Communication</a>
-                <a href="#">IIIT Bhagalpur</a>
+                <Link to="/home">Clean Vision Solutions</Link>
+                <a href="https://www.indiapost.gov.in/" target='blank'>Department of Post</a>
+                <a href="https://dot.gov.in/" target='blank'>Department of Telecommunications</a>
+                <a href="https://iiitbh.ac.in/" target='blank'>IIIT Bhagalpur</a>
               </div>
             </div>
             <div className="footer-column">
               <div className="footer-contact">
                 <h3>Get In Touch</h3>
                 <p>
-                  <i className="fa fa-map-marker" aria-hidden="true"></i> Bhagalpur, Bihar 813210
+                  Indian Institute of Information Technology Bhagalpur,
+                  <br />                 
+                  Bhagalpur, Bihar - 813210
                 </p>
-                <p>
-                  <i className="fa fa-phone" aria-hidden="true"></i> +91 6642XXXXXX
-                </p>
-                <p>
-                  <i className="fa fa-envelope"></i> abcd@example.com
-                </p>
+                <a href='tel:1234567890'>
+                  +91 1234XXXXXX
+                </a>
+                <a href='mailto:sihbgp2024@gmail.com'>
+                  sihbgp2024@gmail.com
+                </a>
                 <div className="footer-social">
                   {/* <a href="#">
                     <i className="fab fa-twitter"></i>
@@ -79,8 +83,8 @@ const Footer = () => {
             <a href="#">Terms of Use</a>
             <a href="#">Privacy Policy</a>
             <a href="#">Cookies</a>
-            <a href="#">Help</a>
-            <a href="#">FAQs</a>
+            <a href="#footer">Help</a>
+            <a href="#faq">FAQs</a>
           </div>
         </div>
         <div className="footer-copyright">
